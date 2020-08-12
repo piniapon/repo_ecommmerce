dcc8066230b77c446a6200f3cedfa0cf54fb28ac
gianfranco quintana <55603293+piniapon@users.noreply.github.com>
2020-08-12 17:57:50 -0300

    price update

--- js/products.js
+++ js/products.js
@@ -18,6 +18,7 @@ function showProductsList(array) {
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
                             <h4 class="mb-1">`+ product.name + `</h4>
+                            <h4 class="mb-1">`+ product.cost + " " + product.currency + `</h4>
                         </div>
                         <p class="mb-1">` + product.description + `</p>
                     </div>
