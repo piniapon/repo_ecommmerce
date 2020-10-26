32a83d8ca1e6f3ea521ca12a174d2bcbe999dc49
gianfranco quintana <55603293+piniapon@users.noreply.github.com>
2020-10-25 22:04:05 -0300

    6ta entrega

--- js/cart.js
+++ js/cart.js
@@ -1,9 +1,15 @@
 var cartArray = [];
 var currency = "UYU"
+var delButtons = document.getElementsByClassName("delBtn");
+var metodoRadio = document.getElementsByName("metodo");
+var creditInput = document.getElementsByClassName("creditInput");
+var transInput = document.getElementsByClassName("transInput");
+
+
 
 toggleCurrency();
 
-//agregar calcSubs
+
 
 function toggleCurrency() {
     document.getElementById("toggleCurrency").addEventListener("click", function () {
@@ -11,9 +17,31 @@ function toggleCurrency() {
             currency = "USD"
         } else { currency = "UYU" }
         document.getElementsByClassName("currency").innerHTML = currency;
+        document.getElementById("toggleCurrency").innerHTML = currency + `<i class="fas fa-exchange-alt"></i>`
         calc()
     });
 }
+
+var envioInputs = document.getElementsByName("envio")
+
+function calcEnvio() {
+    let envio = 0;
+    let subtotal = parseInt(document.getElementById("total").innerText);
+    for (let input of envioInputs) {
+        if (input.checked == true && input.value == "standard") {
+            envio = eval(subtotal + "*" + "0.05")
+        } else if (input.checked == true && input.value == "express") {
+            envio = eval(subtotal + "*" + "0.07")
+        } else if (input.checked == true && input.value == "premium") {
+            envio = eval(subtotal + "*" + "0.15")
+        }
+    }
+
+
+
+    document.getElementById("envioValue").innerHTML = envio
+}
+
 
 function calcSubTotal() {
 
@@ -31,7 +59,6 @@ function calcSubTotal() {
         }
 
         document.getElementById("sub" + i).innerHTML = subtotal;
-        document.getElementById("currency" + i).innerHTML = currency;
 
     }
 }
@@ -43,12 +70,23 @@ function calcTotal() {
         let sub = parseInt(document.getElementById("sub" + i).innerHTML)
         total = total + "+" + sub;
     }
-    document.getElementById("total").innerHTML = eval(total) + currency;
+    document.getElementById("total").innerHTML = eval(total);
+
+}
+
+function calcFinal() {
+    let final = eval(parseInt(document.getElementById("total").innerHTML) + "+" + parseInt(document.getElementById("envioValue").innerHTML));
+    document.getElementById("final").innerHTML = final + currency
+
 }
 
 function calc() {
+
     calcSubTotal()
     calcTotal()
+    calcEnvio()
+    calcFinal()
+
 }
 
 
@@ -73,8 +111,7 @@ function showCart(array) {
         </td>
         <td> <span id="unitCost${i}">${articulo.unitCost} </span>${articulo.currency} </td>
         <td> <span class="totals" id="sub${i}"></span> </td> 
-        <td class="currencyCol">  </td> 
-        <td id="currency${i}"></td>
+        <td><button onClick="del()" class="delBtn" id="${"del" + i}" style="border: 0"><i class="fas fa-trash"></i></button></td>
       </tr>
         `
     }
@@ -83,7 +120,77 @@ function showCart(array) {
 
 }
 
+
+function del() {
+    let index = parseInt(this.id);
+    cartArray.articles.splice(index, 1)
+    showCart(cartArray)
+    calc()
+}
+
 document.addEventListener("DOMContentLoaded", function (e) {
+
+    for (let i = 0; i < metodoRadio.length; i++) {
+        metodoRadio[i].addEventListener("change", function () {
+            if (this.value != "credito") {
+                document.getElementById("credit").style.display = "none"
+                for (let i = 0; i < creditInput.length; i++) {
+                    creditInput[i].removeAttribute("required")
+                    transInput[i].setAttribute("required", true)
+
+                }
+            } else {
+                document.getElementById("credit").style.display = "block"
+                for (let i = 0; i < transInput.length; i++) {
+                    transInput[i].removeAttribute("required")
+                    creditInput[i].setAttribute("required", true)
+
+                }
+            }
+
+            if (this.value != "transferencia") {
+                document.getElementById("transferencia").style.display = "none"
+                for (let i = 0; i < transInput.length; i++) {
+                    transInput[i].removeAttribute("required")
+                    creditInput[i].setAttribute("required", true)
+                }
+            } else {
+                document.getElementById("transferencia").style.display = "block"
+                for (let i = 0; i < creditInput.length; i++) {
+                    creditInput[i].removeAttribute("required")
+                    transInput[i].setAttribute("required", true)
+                }
+            }
+        })
+    }
+
+
+
+
+
+    (function () {
+        'use strict';
+        window.addEventListener('load', function () {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('needs-validation');
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+
+                    if (form.checkValidity() == true) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                        alert("sent")
+                    }
+                });
+            });
+        });
+    })();
 
 
     getJSONData(CART_INFO_URL).then(function (resultObj) {
@@ -93,10 +200,15 @@ document.addEventListener("DOMContentLoaded", function (e) {
             showCart(cartArray)
             calc()
         }
+
+
+
     });
 
-
 });
+
+
+
 
 
 
